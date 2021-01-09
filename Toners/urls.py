44fe255e6d36a59b075818c4dac63d3c6d8d3cc0
Rouinis Seyfallah <rouiniseyf.al@gmail.com>
2021-01-09 03:53:53 +0100

    createing a new view and url to test with

--- Toners/urls.py
+++ Toners/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from django.conf.urls.static import static 
+from django.conf import settings 
 from graphene_django.views import GraphQLView
 from .schema import schema
 
