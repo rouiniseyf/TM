7c94d76f3e10bc1a8f2a103cc0966af56fefb9cd
Rouinis Seyfallah <rouiniseyf.al@gmail.com>
2021-01-09 03:49:51 +0100

    createing a new view and url to test with

--- TonersManagement/urls.py
+++ TonersManagement/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/',.viewsHome),
+    path('',.viewsHome),
     path('toners/',views.Toenrs, name='toners'),
-    path(''.views.hello, name='hello'),
+    path('hello/'.viewshello),
     path('release_voucher/',views.ReleaseVoucherPage, name='release_voucher'),
     path('delete_toner/<str:pk>/', views.delete_toner, name='delete_toner'),
     path('update_toner/<str:pk>/', views.update_toner, name='update_toner'),
