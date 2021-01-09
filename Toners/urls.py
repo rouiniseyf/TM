8aa8e7cd780d57543c9f3ca5db9dc3e75e7ce34c
Rouinis Seyfallah <rouiniseyf.al@gmail.com>
2021-01-09 03:25:14 +0100

    come back urls

--- Toners/urls.py
+++ Toners/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from graphene_django.views import GraphQLView
 from .schema import schema
-from TonersManagement import views 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('TonersManagement.urls')),
-    path('toners/',views.Toenrs, name='toners'),
     path('graphql/', GraphQLView.as_view(graphiql=True)),
     path('api-auth/', include('rest_framework.urls'))
 ]
